0305fc87f2f47a8e7ba4906b038e9f936165b706
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-09-01 13:44:18 -0700

    new styling

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -38,8 +38,8 @@ class UserController < ApplicationController
     @user.name = params[:user][:name]
     @user.bio = params[:user][:bio]
     if params[:user][:user_image]
-      @user.user_image = params[:user][:user_image]
-    end
+    @user.user_image = params[:user][:user_image]
+  end
     @user.user_id = @user.id
     @user.save
     redirect_to user_path(@user)
